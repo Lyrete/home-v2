ac794e96d40eeeea508767d278275ebe84806b11
lyrete <tommi@alajoki.fi>
2024-04-16 18:08:16 +0300

    Change employment to CGI

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ const Home: NextPage = () => {
       <main className="flex flex-col items-center justify-center w-full flex-1 md:px-10 px-5 text-center h-full">
         <div className="text-gray-200 container mx-auto justify-center text-center space-y-5">
           <h1 className="text-5xl md:text-7xl font-bold">Tommi Alajoki</h1>
-          <h2 className="text-l md:text-xl pb-5">Software Developer</h2>
+          <h2 className="text-l md:text-xl pb-5">Software Engineer</h2>
           <p className="leading-loose">
             I build stuff (mostly websites).
             <br />
@@ -32,9 +32,8 @@ const Home: NextPage = () => {
               className="text-blue-500"
               target="_blank"
               rel="noopener noreferrer"
-              href="https://nordhealth.com/"
-            >
-              Nordhealth
+              href="https://www.cgi.com/en/">
+              CGI
             </a>
             .<br />
             For enjoyment in my free time.
@@ -43,55 +42,47 @@ const Home: NextPage = () => {
           <div className="divide-y flex border justify-self-center mx-auto max-w-lg bg-white" />
           <div
             id="logos"
-            className="flex flex-wrap items-center justify-center mx-auto"
-          >
+            className="flex flex-wrap items-center justify-center mx-auto">
             <a
               target="_blank"
               rel="noopener noreferrer"
-              href="https://steamcommunity.com/id/lyrete"
-            >
+              href="https://steamcommunity.com/id/lyrete">
               <FaSteam className={logoClasses} />
             </a>
             <a
               target="_blank"
               rel="noopener noreferrer"
-              href="https://github.com/lyrete"
-            >
+              href="https://github.com/lyrete">
               <FaGithub className={logoClasses} />
             </a>
             <a
               target="_blank"
               rel="noopener noreferrer"
-              href="https://www.linkedin.com/in/tommi-alajoki-561233113/"
-            >
+              href="https://www.linkedin.com/in/tommi-alajoki-561233113/">
               <FaLinkedinIn className={logoClasses} />
             </a>
             <a
               target="_blank"
               rel="noopener noreferrer"
-              href="https://twitter.com/lyrete"
-            >
+              href="https://twitter.com/lyrete">
               <FaTwitter className={logoClasses} />
             </a>
             <a
               target="_blank"
               rel="noopener noreferrer"
-              href="https://twitch.tv/lyrete"
-            >
+              href="https://twitch.tv/lyrete">
               <FaTwitch className={logoClasses} />
             </a>
             <a
               target="_blank"
               rel="noopener noreferrer"
-              href="https://discordapp.com/users/111293089846423552/"
-            >
+              href="https://discordapp.com/users/111293089846423552/">
               <FaDiscord className={logoClasses} />
             </a>
             <a
               target="_blank"
               rel="noopener noreferrer"
-              href="https://www.strava.com/athletes/8419929"
-            >
+              href="https://www.strava.com/athletes/8419929">
               <FaStrava className={logoClasses} />
             </a>
           </div>
